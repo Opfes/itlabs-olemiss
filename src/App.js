ff4fba63b10da719be66ae01455c7f2f4208b78f
dmlindsa <dmlindsa@go.olemiss.edu>
2021-09-08 16:40:16 -0500

    initial code drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import logo from './Resources/om-logo.svg';
+import {isMobile} from "react-device-detect";
+import {Paper , Grid} from "@material-ui/core";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  if (isMobile){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>You're on mobile!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <Grid container
+        direction="column">
+        <Grid item><Paper>
+          <img src={logo} alt="logo" />
+        </Paper></Grid>
+        <Grid item><header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>You're not on mobile!</p>
+        </header></Grid>
+      </Grid>
+    );
+  }
+
+
 }
 
 export default App;
