0317ec77d34474bf3a5a322c06829bc0296e8b4a
dmlindsa <dmlindsa@go.olemiss.edu>
2021-09-08 21:09:25 -0500

    begin building splash page

--- src/App.js
+++ src/App.js
@@ -25,14 +25,25 @@ function App() {
   } else {
     return (
       <Grid container
-        direction="column">
-        <Grid item><Paper>
-          <img src={logo} alt="logo" />
-        </Paper></Grid>
+        direction="column"
+        >
+        <Paper><Grid item container
+        direction="row"
+        alignItems="center"
+        style={{maxWidth: "100%"}}
+        >
+          <Grid item xs={4}><h1 style={{padding: "15px"}}>IT Labs</h1></Grid>
+          <Grid item xs={4}><img src={logo} style={{height: "150px", display: "block", margin:"auto"}} alt="logo" /></Grid>
+          <Grid item xs={4}><h1 style={{padding: "15px", float:"right"}}>Welcome, User</h1></Grid>
+        </Grid></Paper>
         <Grid item><header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>You're not on mobile!</p>
-        </header></Grid>
+          <Grid container
+          direction="column"
+          justifyContent="space-evenly"
+          alignItems="center">
+            <Grid item><img src={logo} className="App-logo" alt="logo" /></Grid>
+            <Grid item><p>IT Labs, from the ground up.</p></Grid>
+        </Grid></header></Grid>
       </Grid>
     );
   }
