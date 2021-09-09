d1fd27b558d66d32c3e57e6683ba896c0bf61d9c
dmlindsa <dmlindsa@go.olemiss.edu>
2021-09-09 16:33:29 -0500

    played around with header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './Resources/om-logo.svg';
+import logo from './Resources/olemisslogo2.png';
 import {isMobile} from "react-device-detect";
 import {Paper , Grid} from "@material-ui/core";
 import './App.css';
@@ -24,27 +24,27 @@ function App() {
     );
   } else {
     return (
-      <Grid container
+      <body>
+        <header className="itlabs-header">
+          <h1 style={{padding: "15px"}}>IT Labs</h1>
+          <img src={logo} style={{height: "100px", display: "block", margin:"-5px", alignSelf:"center"}} alt="logo" />
+          <h1 style={{padding: "15px", float:"right"}}>Welcome, User</h1>
+        </header>
+        <header className="App-header"><Grid container
         direction="column"
         >
-        <Paper><Grid item container
-        direction="row"
-        alignItems="center"
-        style={{maxWidth: "100%"}}
-        >
-          <Grid item xs={4}><h1 style={{padding: "15px"}}>IT Labs</h1></Grid>
-          <Grid item xs={4}><img src={logo} style={{height: "150px", display: "block", margin:"auto"}} alt="logo" /></Grid>
-          <Grid item xs={4}><h1 style={{padding: "15px", float:"right"}}>Welcome, User</h1></Grid>
-        </Grid></Paper>
-        <Grid item><header className="App-header">
+        <Grid item>
           <Grid container
           direction="column"
           justifyContent="space-evenly"
           alignItems="center">
             <Grid item><img src={logo} className="App-logo" alt="logo" /></Grid>
             <Grid item><p>IT Labs, from the ground up.</p></Grid>
-        </Grid></header></Grid>
-      </Grid>
+          </Grid></Grid>
+        </Grid></header>
+      </body>
+      
+      
     );
   }
 
