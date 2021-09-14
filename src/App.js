ffe77ff4d619caed7e4f0c5d34428e4bb7c1f2bb
dmlindsa <dmlindsa@go.olemiss.edu>
2021-09-13 23:36:31 -0500

    changed versioning and revised app file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import { Grid, Paper, makeStyles, TextField, Button } from "@material-ui/core";
-import {isMobile} from "react-device-detect";
-import logo from "./Resources/olemisslogo2.png";
-import { ExitToApp } from "@material-ui/icons";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Landing from "./Landing.js";
 import Dashboard from "./Dashboard.js";
 import './App.css';
 
-const useStyles = makeStyles({
-  mainPaper: {
-    color: "white",
-    backgroundColor: "rgba(255,255,255, 0.8)",
-    borderRadius: "20px",
-    padding: "20px"
-  }, // a style rule
-});
-
-function App() {
-  const classes = useStyles();
-  
+function App() {  
   return (
     <Router>
       <Switch>
